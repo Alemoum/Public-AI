1373003abd23f842a2c62a82456d1ddec5d019e3
Alemoum <marcosvmm3110@gmail.com>
2025-06-11 22:02:25 -0300

    Hardcoding a systemprompt to see how it works

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
-
+    
     verbose = "--verbose" in sys.argv
     args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
 
     if not args:
         print("AI Code Assistant")
-        print('\nUsage: python main.py "your prompt here" [--verbose]')
-        print('Example: python main.py "How do I build a calculator app?"')
+        print('\nUsage: python3 main.py "your prompt here" [--verbose]')
+        print('Example: python3 main.py "How do I build a calculator app?"')
         sys.exit(1)
 
     api_key = os.environ.get("GEMINI_API_KEY")
@@ -33,9 +33,12 @@ def main():
 
 
 def generate_content(client, messages, verbose):
+    system_prompt = f'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt)
     )
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
